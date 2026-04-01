37602d39abb403615d0246ffd208d3c2bdcf1693
GuilhermeMoraesBrantDeAlmeida <gmoraesb@uwo.ca>
2026-04-01 19:13:28 -0400

    we got to the conclusion that the equip interface was redundant. I'm taking it out.

--- Assets/Scripts/Items/Weapon.cs
+++ Assets/Scripts/Items/Weapon.cs
@@ -1,16 +1,11 @@
 using UnityEngine;
 
-public class Weapon : Item, IEquipable
+public class Weapon : Item
 {
     [SerializeField] private int damageBonus = 5;
     [SerializeField] private float attackRangeBonus = 0.25f;
 
     protected override void ApplyTo(Michael michael)
-    {
-        Equip(michael);
-    }
-
-    public void Equip(Michael michael)
     {
         if (michael == null) return;
 
