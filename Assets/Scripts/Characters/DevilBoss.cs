2c44de74775238076839ed207716d6f00e69088f
GJassi <gjassi101@gmail.com>
2026-04-07 18:54:53 -0400

    Progress on boss script and boss fight room

--- Assets/Scripts/Characters/DevilBoss.cs
+++ Assets/Scripts/Characters/DevilBoss.cs
@@ -1,16 +1,6 @@
 using UnityEngine;
 
-public class DevilBoss : MonoBehaviour
+public class DevilBoss : Enemy
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
